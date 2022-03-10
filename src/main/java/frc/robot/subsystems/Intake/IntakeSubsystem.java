23e0a6114db7a93db1440ee1f5e8a11a3181efb6
Henry Scheible <henry.scheible@gmail.com>
2022-03-10 17:26:24 -0600

    Change intake to use IOLayer

--- src/main/java/frc/robot/subsystems/Intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/Intake/IntakeSubsystem.java
@@ -2,16 +2,13 @@ package frc.robot.subsystems.Intake;
 
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import lib.iotemplates.OpenLoopIO;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-
 import lib.iotemplates.OpenLoopIOTalonSRXBase;
+import lib.iotemplates.SolenoidIO;
+import lib.iotemplates.SolenoidIOBase;
 import org.littletonrobotics.junction.Logger;
 
 public class IntakeSubsystem extends SubsystemBase {
@@ -19,13 +16,10 @@ public class IntakeSubsystem extends SubsystemBase {
     private final OpenLoopIOTalonSRXBase motorIO = new OpenLoopIOTalonSRXBase(Constants.IntakeConstants.kCANPort);
     private final OpenLoopIO.OpenLoopIOInputs motorInputs = new OpenLoopIO.OpenLoopIOInputs();
 
+    private final SolenoidIOBase solenoidIO = new SolenoidIOBase(6, 7);
+    private final SolenoidIO.SolenoidIOInputs solenoidInputs = new SolenoidIO.SolenoidIOInputs();
+
     double speed = 0;
-
-    private final DoubleSolenoid intakeSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, 6, 7);
-
-    public IntakeSubsystem() {
-
-    }
 
     public void spinIntake() {
         motorIO.setVoltage(-12 * speed);
@@ -47,16 +41,18 @@ public class IntakeSubsystem extends SubsystemBase {
     }
 
     public void intakeOut(){
-        intakeSolenoid.set(DoubleSolenoid.Value.kForward);
+        solenoidIO.set(DoubleSolenoid.Value.kForward);
     }
 
     public void intakeIn(){
-        intakeSolenoid.set(DoubleSolenoid.Value.kReverse);
+        solenoidIO.set(DoubleSolenoid.Value.kReverse);
     }
 
     public void periodic() {
        motorIO.updateInputs(motorInputs);
        Logger.getInstance().processInputs("IntakeMotor", motorInputs);
+       solenoidIO.updateInputs(solenoidInputs);
+       Logger.getInstance().processInputs("IntakeSolenoid", solenoidInputs);
     }
 }
 
