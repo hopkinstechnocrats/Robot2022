63e057180c2691ef1e291d71d7aee7391f39490d
technocrats-software <97137446+technocrats-software@users.noreply.github.com>
2022-03-10 21:01:59 -0600

    Update LimelightSubsystem.java

--- src/main/java/frc/robot/subsystems/Limelight/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/Limelight/LimelightSubsystem.java
@@ -6,21 +6,28 @@ package frc.robot.subsystems.Limelight;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class LimelightSubsystem extends SubsystemBase {
   /** Creates a new LimelightSubsystem. */
+  double P = 1;
+  double I, D = 0;
+  double period = 0.1;
+  PIDController aiming = new PIDController(P, I, D, period);
+
   public LimelightSubsystem() {
     NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
     NetworkTableEntry tx = table.getEntry("tx");
     //NetworkTableEntry ty = table.getEntry("ty");
+    
 
-    double x = tx.getDouble(0.0);
+    double horizontalAngle = tx.getDouble(0.0);
     //double y = ty.getDouble(0.0);
     
-    SmartDashboard.putNumber("LimelightX", x);
+    SmartDashboard.putNumber("LimelightX", horizontalAngle);
     //SmartDashboard.putNumber("LimelightY", y);
     }
 
