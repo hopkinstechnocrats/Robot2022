cd07aff34505625bec56c611edc43e7cf307437b
Henry Scheible <henry.scheible@gmail.com>
2022-03-11 21:06:46 -0600

    Make Linear Interpolator Test

---
+++ src/test/java/lib/LinearInterpolatorTest.java
@@ -0,0 +1,16 @@
+package lib;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+class LinearInterpolatorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
